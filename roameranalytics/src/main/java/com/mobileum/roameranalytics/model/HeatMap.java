cd301152e40d647da9aa667192d51b923d0856cb
smruti <smruti.sahoo@quovantis.com>
2014-08-19 19:04:41 +0530

    Trend Home page model implemented

---
+++ roameranalytics/src/main/java/com/mobileum/roameranalytics/model/HeatMap.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package com.mobileum.roameranalytics.model;
+
+/**
+ * @author smruti
+ *
+ */
+public class HeatMap {
+	
+	String countryCode;
+	float moUsage;
+	float mtUsage;
+	float dataUsage;
+	float smsUsage;
+	float usagePojection;
+	
+	/**
+	 * @return the countryCode
+	 */
+	public String getCountryCode() {
+		return countryCode;
+	}
+	/**
+	 * @param countryCode the countryCode to set
+	 */
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	/**
+	 * @return the moUsage
+	 */
+	public float getMoUsage() {
+		return moUsage;
+	}
+	/**
+	 * @param moUsage the moUsage to set
+	 */
+	public void setMoUsage(float moUsage) {
+		this.moUsage = moUsage;
+	}
+	/**
+	 * @return the mtUsage
+	 */
+	public float getMtUsage() {
+		return mtUsage;
+	}
+	/**
+	 * @param mtUsage the mtUsage to set
+	 */
+	public void setMtUsage(float mtUsage) {
+		this.mtUsage = mtUsage;
+	}
+	/**
+	 * @return the dataUsage
+	 */
+	public float getDataUsage() {
+		return dataUsage;
+	}
+	/**
+	 * @param dataUsage the dataUsage to set
+	 */
+	public void setDataUsage(float dataUsage) {
+		this.dataUsage = dataUsage;
+	}
+	/**
+	 * @return the smsUsage
+	 */
+	public float getSmsUsage() {
+		return smsUsage;
+	}
+	/**
+	 * @param smsUsage the smsUsage to set
+	 */
+	public void setSmsUsage(float smsUsage) {
+		this.smsUsage = smsUsage;
+	}
+	/**
+	 * @return the usagePojection
+	 */
+	public float getUsagePojection() {
+		return usagePojection;
+	}
+	/**
+	 * @param usagePojection the usagePojection to set
+	 */
+	public void setUsagePojection(float usagePojection) {
+		this.usagePojection = usagePojection;
+	}
+	
+	
+
+}
