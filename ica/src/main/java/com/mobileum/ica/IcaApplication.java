44431427056c55b2d2634542011cba8bbba76260
ashish-quo <ashish.arora@quovantis.com>
2017-09-13 13:46:41 +0530

    Initial Commit for ica.

---
+++ ica/src/main/java/com/mobileum/ica/IcaApplication.java
@@ -0,0 +1,12 @@
+package com.mobileum.ica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcaApplication.class, args);
+	}
+}
